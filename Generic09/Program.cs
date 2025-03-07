8f91217ad54a409d837dfa8fa26c745cb02c5f8e
ST10401917 <ST10401917@vcconnect.edu.za>
2025-03-07 14:43:12 +0200

    Add project files.

---
+++ Generic09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic09 // push to GitHub
+{
+    class GenericDictionaryExample
+    {
+        public static void Main()
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>
+            {
+                {1, "John" },
+                {2, "Emma" }, // only accept specific Data type integer and String
+                {3, "Luke" },
+                {4, "Sam" }
+            };
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"ID: {student.Key}, Name: {student.Value}");
+            }
+        }
+    }
+}
